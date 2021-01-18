e963574969241d9cd58d3a8252aa131f2ff0a983
Anson Avellar <anson@ansonavellar.com>
2021-01-18 16:46:32 -0500

    we have added report closed functionality

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 const NavBar = ( {logInSubmitHandler, userStatus, userName, logoutHandler, signUpPop, signUpButtonHandler, createUser} ) => {
       // if user is logged in, we should render a new nav bar welcoming back the user.  logInSubmitHandler={logInSubmitHandler} 
       console.log('signup?', signUpPop)
+  // check if user is logged in and if they have pressed to sign up
    if (!userStatus && !signUpPop) {
     return (
       <div className="login">
-        <form >
+        <form>
           <input className="loginput" id ="userName" type="text"
           placeholder="username"
           /> 
@@ -28,10 +29,9 @@ const NavBar = ( {logInSubmitHandler, userStatus, userName, logoutHandler, signU
         </button>
         <button id="signup" type="button" onClick={() => signUpButtonHandler()}>Sign Up</button>
         </form>
-            {/* {seen ? <SignUp toggle={this.togglePop} /> : null} */}
-            {/* {!signUpPop ? <SignUp /> : null} */}
       </div>  
     )
+    // if user has pressed sign up button, we return sign up form 
   } else if (signUpPop) {
     return (
       <div className="signupform">Please sign up
@@ -47,24 +47,21 @@ const NavBar = ( {logInSubmitHandler, userStatus, userName, logoutHandler, signU
           /> 
           </div>
           </form>
-          {/* // login event handler */}
           <div>
+            {/* say hello to our little panda 🐼 */}
           <button id="signupformbtn" type="button" onClick={() => {
             const username = document.getElementById('userName').value;
             const password = document.getElementById('passWord').value; 
-            // if (!password || !username) {
-            //   // lets render something on the page instead of an alert
-            //   alert('Please enter username and password!')
-            // } else {
               createUser(username, password);
           } }>Sign Up</button>
           </div>
       </div>
     )
+    // otherwise we return a welcome home message
   } else {
     return (
       <div className="loggedin">
-        Welcome back, {userName}
+        Welcome home, {userName}
         <button id="logbutton" type="button" onClick={() => logoutHandler()
         }>Log Out</button>
       </div>
