eec14122e899344563b9e3b5a3926f07f3dc2fee
Anson Avellar <anson@ansonavellar.com>
2021-01-17 20:58:21 -0500

    daniel is allowing me to send updates

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
-
+const NavBar = ( {logInSubmitHandler} ) => {
   return (
     <div className="login">
-          <Link to='/login'>
-            <img id='login' src="./assets/user-login-icon-14.png" height='75px' width='75px'/>
-          </Link>
-          <Link to='/signup'>
-            <img id='signup' src="./assets/signup.png" height='75px' width='75px'/>
-          </Link>
-      {/* <a className="login" onClick={this.togglePopup} >
-              Login / Create Account
-            {this.state.showPopup ?  <Popup closePopup={this.togglePopup} />  
-  : null  */}
+          {/* <Link to='/login'><img id='login' src="./assets/user-login-icon-14.png" ></img></Link> */}
+      <form logInSubmitHandler={logInSubmitHandler}>
+        <input id="userName" type="text"
+        placeholder="user name"
+        /> 
+        <input id="passWord" type="text"
+          placeholder="password"
+        /> 
+        <button type="button" onClick={() => {
+          const username = document.getElementById('userName').value;
+          const password = document.getElementById('passWord').value; 
+          if (!passWord || !userName) {
+            alert('Please enter username and password!')
+          }
+          logInSubmitHandler(username, password);
+        }}>
+          Log In
+      </button>
+      </form>
+          <Link to='/signup'><img id='signup' src="./assets/signup.png" ></img></Link>
+      {/* <form value={state.value} onSubmit={handleSubmit}>
+      <input className="search" placeholder="search..." type="text" value={state.value} onChange={handleSubmit} />
+      <button type="button" value={state.value} onClick={handleClick}>
+      </button> */}
     </div>  
   )};
   
